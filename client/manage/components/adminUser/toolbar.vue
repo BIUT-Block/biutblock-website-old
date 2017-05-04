09e11ff4edbebd077ff128d5a9c21ef6293f1a43
doramart <idorothy@hotmail.com>
2017-05-05 00:07:42 +0800

    调试模态框

--- client/manage/components/adminUser/toolbar.vue
+++ client/manage/components/adminUser/toolbar.vue
@@ -1,23 +1,32 @@
 <template>
     <div class="dr-toolbar">
-        <el-button size="small" type="primary" icon="plus">添加用户</el-button>
+        <el-button size="small" type="primary" icon="plus" @click="addUser">添加用户</el-button>
         <div class="dr-searchInput">
             <el-input size="small" placeholder="请填写关键字" icon="search" v-model="input2" :on-icon-click="handleIconClick">
             </el-input>
         </div>
+        <UserForm :show="formState"></UserForm>
     </div>
 </template>
 <script>
+    import UserForm from './userForm'
     export default {
         data() {
             return {
+                formState: false,
                 input2: ''
             }
         },
         methods: {
             handleIconClick(ev) {
                 console.log(ev);
+            },
+            addUser() {
+                this.formState = true;
             }
+        },
+        components: {
+            UserForm
         }
 
     }
