af4f9fa55fa2310bbab5085e718f915c8203c724
xiaoshen746 <doramart@qq.com>
2017-07-06 17:40:44 +0800

    静态文件压缩

--- src/index/store/services.js
+++ src/index/store/services.js
@@ -1,73 +1,71 @@
 import Axios from "axios"
 import api from '~api'
 if (typeof window == "undefined") {
-    Axios.defaults.baseURL = 'http://127.0.0.1:8080';
+    Axios.defaults.baseURL = 'http://127.0.0.1:8080'
 }
 
 export function reqJsonData(url, params = {}, method = 'post') {
 
     if (method === 'get') {
-        return Axios.get('/' + url, {
-            params
-        })
+        return api.get('/' + url, params)
     } else if (method === 'post') {
-        return Axios.post('/' + url, params)
+        return api.post('/' + url, params)
     }
 
 }
 export default {
 
     getUserSession() {
-        return reqJsonData('api/users/session', {}, 'get')
+        return reqJsonData('users/session', {}, 'get')
     },
 
     userLogOut() {
-        return reqJsonData('api/users/logOut', {}, 'get')
+        return reqJsonData('users/logOut', {}, 'get')
     },
 
     contentList(params) {
         // console.log('---begin to get contents----', params);
-        return reqJsonData('api/content/getList', params, 'get')
+        return reqJsonData('content/getList', params, 'get')
     },
 
     getSimpleList(params) {
-        return reqJsonData('api/content/getSimpleListByParams', params, 'get')
+        return reqJsonData('content/getSimpleListByParams', params, 'get')
     },
 
     getOneContent(params) {
-        return reqJsonData('api/content/getContent', params, 'get')
+        return reqJsonData('content/getContent', params, 'get')
     },
 
     userDoLogin(params) {
-        return reqJsonData('api/users/doLogin', params)
+        return reqJsonData('users/doLogin', params)
     },
 
     adminDoLogin(params) {
-        return reqJsonData('api/admin/doLogin', params)
+        return reqJsonData('admin/doLogin', params)
     },
 
     contentCategoryList(params) {
-        return reqJsonData('api/contentCategory/getList', params, 'get')
+        return reqJsonData('contentCategory/getList', params, 'get')
     },
 
     contentTagList(params) {
-        return reqJsonData('api/contentTag/getList', params, 'get')
+        return reqJsonData('contentTag/getList', params, 'get')
     },
 
     userMessageList(params) {
-        return reqJsonData('api/message/getList', params, 'get')
+        return reqJsonData('message/getList', params, 'get')
     },
 
     userSendMessage(params) {
-        return reqJsonData('api/message/post', params)
+        return reqJsonData('message/post', params)
     },
 
     siteMapList() {
-        return reqJsonData('api/sitemap/getList', {}, 'get')
+        return reqJsonData('sitemap/getList', {}, 'get')
     },
 
     getSystemConfigs(params) {
-        return reqJsonData('api/systemConfig/getConfig', params, 'get')
+        return reqJsonData('systemConfig/getConfig', params, 'get')
     }
 
 }
