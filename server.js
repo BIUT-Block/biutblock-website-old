18a6e89d21fe5feb38ffa30a7659808c3a917ce4
xiaoshen746 <doramart@qq.com>
2017-07-19 15:21:18 +0800

    test

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.use(session({ //session持久化配置
 // 鉴权用户
 app.use(authUser.auth);
 // 初始化首页菜单
-app.use(renderCates);
+// app.use(renderCates);
 // 设置 express 根目录
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')))
