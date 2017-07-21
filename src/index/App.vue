9e1f469e6d259b8b57910990e7b254a03cdcbbc7
xiaoshen746 <doramart@qq.com>
2017-07-21 12:41:00 +0800

    静态文件压缩

--- src/index/App.vue
+++ src/index/App.vue
@@ -1,17 +1,14 @@
 <style lang="scss">
-    @import './assets/base.css';
-    @import '../../node_modules/element-ui/lib/theme-default/index.css';
-    @import '../../node_modules/font-awesome/css/font-awesome.min.css';
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: all .2s ease;
-    }
+@import './assets/base.css';
+.fade-enter-active,
+.fade-leave-active {
+    transition: all .2s ease;
+}
 
-    .fade-enter,
-    .fade-leave-active {
-        opacity: 0;
-    }
-
+.fade-enter,
+.fade-leave-active {
+    opacity: 0;
+}
 </style>
 <template>
     <div id="app">
@@ -27,35 +24,35 @@
     </div>
 </template>
 <script>
-    import {
-        mapGetters,
-        mapActions
-    } from 'vuex'
-    import MyHeader from './components/header'
-    import MyFooter from './components/Footer'
-    import services from './store/services'
-    export default {
+import {
+    mapGetters,
+    mapActions
+} from 'vuex'
+import MyHeader from './components/header'
+import MyFooter from './components/Footer'
+import services from './store/services'
+export default {
 
-        data() {
-            return {
-                navItems: []
-            }
-        },
-        components: {
-            MyHeader,
-            MyFooter
-        },
-        computed: {
-            ...mapGetters([
-                'simplePage'
-            ])
-        },
-        methods: {
-
-        },
-        beforeMount() {
-            this.$store.dispatch('normalPage');
+    data() {
+        return {
+            navItems: []
         }
+    },
+    components: {
+        MyHeader,
+        MyFooter
+    },
+    computed: {
+        ...mapGetters([
+            'simplePage'
+        ])
+    },
+    methods: {
+
+    },
+    beforeMount() {
+        this.$store.dispatch('normalPage');
     }
+}
 
 </script>
