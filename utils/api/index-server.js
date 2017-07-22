ef07e6a9c1af7dfdb6c009d46534395d2598ed25
xiaoshen746 <doramart@qq.com>
2017-07-22 17:27:29 +0800

    test

--- utils/api/index-server.js
+++ utils/api/index-server.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 import md5 from 'md5'
 import config from './config-server'
-import logUtil from '../logUtil'
+const logUtil = require('../logUtil')
 const SSR = global.__VUE_SSR_CONTEXT__
 const cookies = SSR.cookies || {}
 const username = cookies.username || ''
