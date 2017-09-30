89de8d3cadfe65cdd024d210ced95db96c58c921
xiaoshen746 <doramart@qq.com>
2017-09-30 18:17:33 +0800

    test

--- src/manage/components/common/TopBar.vue
+++ src/manage/components/common/TopBar.vue
@@ -2,40 +2,40 @@
     <div class="dr-toolbar">
         <div class="option-button">
             <div v-if="type === 'adminGroup'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addRole">添加角色</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addRole">添加角色</el-button>
             </div>
             <div v-else-if="type === 'adminUser'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addUser">添加管理员</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addUser">添加管理员</el-button>
             </div>
             <div v-else-if="type === 'adminResource'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addResource">添加主菜单</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addResource">添加主菜单</el-button>
             </div>
             <div v-else-if="type === 'content'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addContent">添加文档</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addContent">添加文档</el-button>
             </div>
             <div v-else-if="type === 'contentCategory'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addTopCates">添加主分类</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addTopCates">添加主分类</el-button>
             </div>
             <div v-else-if="type === 'contentMessage'">
-                <el-button size="small" type="danger" suffix-icon="el-icon-delete" @click="branchDelete('msg')">批量删除</el-button>
+                <el-button size="small" type="danger" icon="el-icon-delete" @click="branchDelete('msg')">批量删除</el-button>
             </div>
             <div v-else-if="type === 'contentTag'">
-                <el-button size="small" type="primary" suffix-icon="el-icon-plus" @click="addTag">添加新标签</el-button>
+                <el-button size="small" type="primary" icon="el-icon-plus" @click="addTag">添加新标签</el-button>
             </div>
             <div v-else-if="type === 'regUser'">
-                <el-button size="small" type="danger" suffix-icon="el-icon-delete" @click="branchDelete('user')">批量删除</el-button>
+                <el-button size="small" type="danger" icon="el-icon-delete" @click="branchDelete('user')">批量删除</el-button>
             </div>
             <div v-else-if="type === 'backUpData'">
                 <el-button size="small" type="primary" @click="bakUpData" :loading="loadingState">
                     <i class="el-icon-upload"></i> &nbsp;备份数据库</el-button>
             </div>
             <div v-else-if="type === 'systemOptionLogs'">
-                <el-button size="small" type="danger" suffix-icon="el-icon-delete" @click="branchDelete('systemlogs')">批量删除</el-button>
-                <el-button size="small" type="warning" suffix-icon="el-icon-warning" @click="clearSystemOptionLogs">清空日志</el-button>
+                <el-button size="small" type="danger" icon="el-icon-delete" @click="branchDelete('systemlogs')">批量删除</el-button>
+                <el-button size="small" type="warning" icon="el-icon-warning" @click="clearSystemOptionLogs">清空日志</el-button>
             </div>
             <div v-else-if="type === 'systemNotify'">
                 <el-button size="small" @click="setHasRead()">标记为已读</el-button>
-                <el-button size="small" type="danger" suffix-icon="el-icon-delete" @click="branchDelete('systemnotify')">批量删除</el-button>
+                <el-button size="small" type="danger" icon="el-icon-delete" @click="branchDelete('systemnotify')">批量删除</el-button>
             </div>
             <div v-else-if="type === 'systemAnnounce'">
                 <el-button type="primary" size="small" @click="addSysAnnounce">
