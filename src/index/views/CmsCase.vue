5dd3bb7d19afd2283d163109bfeda9c2cb6ff295
xiaoshen746 <doramart@qq.com>
2017-11-08 10:02:42 +0800

    修改部分样式

--- src/index/views/CmsCase.vue
+++ src/index/views/CmsCase.vue
@@ -8,7 +8,13 @@
                 </el-col>
                 <el-col :xs="22" :sm="22" :md="18" :lg="18">
                     <el-row :gutter="20">
-                      <h3 style="margin:5px auto 20px;">这些站点在用DoraCMS：</h3>
+                      <h3>
+                          这些站点在用DoraCMS：
+                          <el-button-group>
+                            <el-button size="mini" @click="getToStar" type="primary" plain icon="el-icon-star-on">Star</el-button>
+                            <el-button size="mini" @click="getToStar" style="color:#2D2F33" plain>{{cmsStar}}</el-button>
+                          </el-button-group>              
+                      </h3>
                       <AdsPannel id="BkxSmqcaAZ" />
                     </el-row>
                 </el-col>
@@ -23,6 +29,15 @@
 <style lang="scss">
 .case-box {
   min-height: 400px;
+  h3 {
+    margin: 5px 10px 20px;
+    font-size: 15px;
+    color: #878d99;
+    .el-button--mini {
+      padding: 5px 8px;
+      font-weight: 700;
+    }
+  }
   img {
     width: 100%;
     height: 2rem;
@@ -32,10 +47,33 @@
 
 <script>
 import AdsPannel from "../components/AdsPannel.vue";
+import axios from "axios";
+
 export default {
   name: "case-item",
   components: {
     AdsPannel
+  },
+  data() {
+    return {
+      cmsStar: 1,
+      stargazersUrl: "https://github.com/doramart/DoraCMS/stargazers"
+    };
+  },
+  methods: {
+    getToStar() {
+      window.open(this.stargazersUrl);
+    }
+  },
+  mounted() {
+    axios
+      .get("https://api.github.com/repos/doramart/DoraCMS")
+      .then(response => {
+        this.cmsStar = response.data.stargazers_count;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 };
 </script>
