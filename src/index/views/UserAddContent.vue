dacfaf876d10bc4cd03a1e6d6a9c031d044173bb
肖燊 <dora@xiaoshendeMini.lan>
2017-12-24 20:43:00 +0800

    添加用户模块

--- src/index/views/UserAddContent.vue
+++ src/index/views/UserAddContent.vue
@@ -8,9 +8,16 @@
         <el-col :xs="22" :sm="22" :md="22" :lg="19" :xl="14">
           <div class="user-info">
             <el-row :gutter="15" class="basic-info">
+                <el-form :model="contentFormState.formData" :rules="rules" ref="ruleForm" label-width="70px" class="content-info-form">
+
               <el-col :xs="19" :sm="19" :md="19" :lg="19" :xl="19" class="right-pannel">
                 {{contentFormState}}
-                  <el-form :model="contentFormState.formData" :rules="rules" ref="ruleForm" label-width="70px" class="info-form">
+                    <el-form-item label="分类" prop="categories">
+                      <el-cascader size="small" expand-trigger="hover" :options="cateMenu.docs"  @change="handleChangeCategory" :props="categoryProps">
+                      </el-cascader>
+                    </el-form-item>
+                    
+                    
                     <el-form-item label="标题" prop="title">
                       <el-input placeholder="请输入标题" v-model="contentFormState.formData.title"></el-input>
                     </el-form-item>
@@ -22,15 +29,30 @@
                           <textarea style="display:none;">### Hello Editor.md !</textarea>
                       </div>
                     </el-form-item>
-                    <el-form-item>
-                      <el-button size="small" type="primary" @click="submitForm('ruleForm')">提交发布</el-button>
-                    </el-form-item>
-                  </el-form>
+                    
               </el-col>
               <el-col :xs="5" :sm="5" :md="5" :lg="5" :xl="5" class="left-pannel">
-               <el-cascader size="small" expand-trigger="hover" :options="cateMenu.docs"  @change="handleChangeCategory" :props="categoryProps">
-                </el-cascader>
+              <el-form-item>
+                <el-button size="small" type="primary" @click="submitForm('ruleForm')">提交发布</el-button>
+              </el-form-item>
+               <PannelBox title="缩略图" className="markdown-pannle">
+                  <el-form-item prop="sImg">
+                        <el-upload class="avatar-uploader" action="/system/upload?type=images" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                            <img v-if="contentFormState.formData.sImg" :src="contentFormState.formData.sImg" class="avatar">
+                            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                        </el-upload>
+                    </el-form-item>
+              </PannelBox>
+               <PannelBox title="标签" className="markdown-pannle">
+                 <el-form-item prop="tags">
+                        <el-select v-model="contentFormState.formData.tags" multiple filterable allow-create placeholder="请选择文章标签">
+                            <el-option v-for="item in contentTagList.data" :key="item._id" :label="item.name" :value="item._id">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+               </PannelBox>
               </el-col>
+              </el-form>
             </el-row>
           </div>
         </el-col>
@@ -45,10 +67,10 @@
 </template>
 <script>
 import api from "~api";
-import UserCenterLeftMenu from "../components/UserCenterLeftMenu";
+// import UserCenterLeftMenu from "../components/UserCenterLeftMenu";
 const validatorUtil = require("../../../utils/validatorUtil.js");
 import { renderTreeData } from "../../manage/store/actions";
-
+import PannelBox from "../components/PannelBox.vue";
 import { mapGetters, mapActions } from "vuex";
 export default {
   name: "userLogin",
@@ -58,21 +80,43 @@ export default {
     };
   },
   components: {
-    UserCenterLeftMenu
+    // UserCenterLeftMenu,
+    PannelBox
   },
   data() {
     return {
       rules: {
-        password: [
+        title: [
           {
             required: true,
-            message: "请输入密码",
+            message: "请输入文档标题",
             trigger: "blur"
           },
           {
+            min: 5,
+            max: 50,
+            message: "5-50个非特殊字符",
+            trigger: "blur"
+          }
+        ],
+        stitle: [
+          {
+            required: true,
+            message: "请输入简短标题",
+            trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 40,
+            message: "5-40个非特殊字符",
+            trigger: "blur"
+          }
+        ],
+        categories: [
+          {
             validator: (rule, value, callback) => {
-              if (!validatorUtil.checkPwd(value)) {
-                callback(new Error("6-12位，只能包含字母、数字和下划线!"));
+              if (_.isEmpty(value)) {
+                callback(new Error("请选择文档类别!"));
               } else {
                 callback();
               }
@@ -80,20 +124,28 @@ export default {
             trigger: "blur"
           }
         ],
-        confirmPassword: [
-          {
-            required: true,
-            message: "请确认密码",
-            trigger: "blur"
-          },
+        tags: [
           {
             validator: (rule, value, callback) => {
-              if (value !== this.contentFormState.formData.password) {
-                callback(new Error("两次输入密码不一致!"));
+              if (_.isEmpty(value)) {
+                callback(new Error("请选择标签!"));
               } else {
                 callback();
               }
             },
+            trigger: "change"
+          }
+        ],
+        discription: [
+          {
+            required: true,
+            message: "请输入内容摘要",
+            trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 300,
+            message: "5-100个非特殊字符",
             trigger: "blur"
           }
         ]
@@ -109,6 +161,28 @@ export default {
     handleChangeCategory(value) {
       console.log(value);
     },
+    handleAvatarSuccess(res, file) {
+      let imageUrl = res;
+      this.$store.dispatch("showContentForm", {
+        edit: this.contentFormState.edit,
+        formData: Object.assign({}, this.contentFormState.formData, {
+          sImg: imageUrl
+        })
+      });
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isPNG = file.type === "image/png";
+      const isGIF = file.type === "image/gif";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isJPG && !isPNG && !isGIF) {
+        this.$message.error("上传头像图片只能是 JPG,PNG,GIF 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return (isJPG || isPNG || isGIF) && isLt2M;
+    },
     setFormLogo(res) {
       this.contentFormState && (this.contentFormState.formData.logo = res);
     },
@@ -116,7 +190,7 @@ export default {
       let userContent = postEditor.getMarkdown();
       this.$refs[formName].validate(valid => {
         if (valid) {
-          if (!content) {
+          if (!userContent) {
             this.$message.error("请输入内容");
           } else {
             let params = this.contentFormState.formData;
@@ -153,7 +227,8 @@ export default {
   },
   computed: {
     ...mapGetters({
-      contentFormState: "frontend/user/contentFormState"
+      contentFormState: "frontend/user/contentFormState",
+      contentTagList: "global/tags/getTagList"
     }),
     cateMenu() {
       let fullNav = this.$store.getters["global/category/getHeaderNavList"];
@@ -163,6 +238,7 @@ export default {
   },
   mounted() {
     let _this = this;
+    this.$store.dispatch("global/tags/getTagList", { limit: 100 });
     window.postEditor = editormd("post-content", {
       width: "100%",
       height: 500,
