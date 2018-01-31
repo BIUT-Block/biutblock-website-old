2ba3cdeebc79a858ebc6ebc8699c1acbe1e39d8c
xiaoshen746 <doramart@qq.com>
2018-01-31 19:14:51 +0800

    test

--- utils/settings.js
+++ utils/settings.js
@@ -18,7 +18,7 @@ module.exports = {
     //    数据库配置
     URL: 'mongodb://127.0.0.1:27017/secblock',
     DB: 'secblock',
-    HOST: '103.71.237.43',
+    HOST: '127.0.0.1',
     PORT: 27017,
     USERNAME: 'secblockio',
     PASSWORD: 'secblock123Io',
@@ -33,7 +33,7 @@ module.exports = {
     assetsCdn: true, // 静态资源使用cnd.请在build完成后将 elemt.*.js 上传的七牛的融合cdn
 
     // redis配置
-    openRedis: false, //是否开启,若为true 则下面的信息必须配置正确完整
+    openRedis: true, //是否开启,若为true 则下面的信息必须配置正确完整
     redis_host: '127.0.0.1',
     redis_port: 6379,
     redis_psd: 'HuiPing520@Xiaoshen@520',
