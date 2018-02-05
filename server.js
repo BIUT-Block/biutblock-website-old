09248215f210c2b9965e2176d802583fe01f0330
xiaoshen746 <doramart@qq.com>
2018-02-05 17:06:58 +0800

    test

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ bot.on('text', async ({ message, replyWithHTML }) => {
                         let currentLink = "https://www.secblock.io/referral?code=" + currentCode;
                         let shareWords = "Your verification code: " + myWallet.myCode + ", SUCCESS & DONE!Join the SEC\u2019s official Telegram group to get 20 SEC awards; Each successful invitation to a user can also receive an additional 20 SEC awards, with a maximum of 600SEC per person, waiting to be dropped.Your share link: " + currentLink + "<br/>你的验证码是: " + myWallet.myCode + "，校验成功！加入SEC官方Telegram群即可获得20个SEC奖励；每成功邀请一个用户，还可额外获得20个SEC奖励，每人最多可得600SEC，坐等空投，送完即止。你的分享链接：" + currentLink + "";
                         console.log('---success---', shareWords);
-                        replyWithHTML('<a>' + shareWords + '</a>')
+                        replyWithHTML(shareWords)
                         // 标记已关注群并发送
                         await SecCandyLog.activeUserWallet(currentCode);
                     }
