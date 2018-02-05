e226a57e002afc0cab55d5b6bfd35e486d24c5c3
xiaoshen746 <doramart@qq.com>
2018-02-05 17:10:13 +0800

    tet

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ bot.on('text', async ({ message, replyWithHTML }) => {
                         replyWithHTML('Your code: ' + myWallet.myCode + ', Failed. Each Telegram user can only be verified once. 你的验证码：' + myWallet.myCode + '，校验失败，每个Telegram用户仅可校验一次。')
                     } else {
                         let currentLink = "https://www.secblock.io/referral?code=" + currentCode;
-                        let shareWords = "Your verification code: " + myWallet.myCode + ", SUCCESS & DONE!Join the SEC\u2019s official Telegram group to get 20 SEC awards; Each successful invitation to a user can also receive an additional 20 SEC awards, with a maximum of 600SEC per person, waiting to be dropped.Your share link: " + currentLink + "<br/>你的验证码是: " + myWallet.myCode + "，校验成功！加入SEC官方Telegram群即可获得20个SEC奖励；每成功邀请一个用户，还可额外获得20个SEC奖励，每人最多可得600SEC，坐等空投，送完即止。你的分享链接：" + currentLink + "";
+                        let shareWords = "Your verification code: " + myWallet.myCode + ", SUCCESS & DONE!Join the SEC\u2019s official Telegram group to get 20 SEC awards; Each successful invitation to a user can also receive an additional 20 SEC awards, with a maximum of 600SEC per person, waiting to be dropped.Your share link: " + currentLink + ". 你的验证码是: " + myWallet.myCode + "，校验成功！加入SEC官方Telegram群即可获得20个SEC奖励；每成功邀请一个用户，还可额外获得20个SEC奖励，每人最多可得600SEC，坐等空投，送完即止。你的分享链接：" + currentLink + "";
                         console.log('---success---', shareWords);
                         replyWithHTML(shareWords)
                         // 标记已关注群并发送
