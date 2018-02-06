29ea6520d5e1b696f75a9c47cde1e1ccf9aaba68
xiaoshen746 <doramart@qq.com>
2018-02-06 09:58:04 +0800

    样式修改

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var jsmin = require("gulp-uglify");
+var cssmin = require("gulp-minify-css");
 
 var doraWhiteSassPath = './src/index/dorawhite/css/main.scss';
 var doraWhiteCssPath = './public/themes/dorawhite/css';
@@ -10,6 +12,7 @@ var doraWhiteCssPath = './public/themes/dorawhite/css';
 gulp.task('sass', function () {
     return gulp.src(doraWhiteSassPath)
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
         .pipe(gulp.dest(doraWhiteCssPath));
 });
 
