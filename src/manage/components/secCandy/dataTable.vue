b0df834f5d8f14f8c0de8d262e220e392a7e9171
xiaoshen746 <doramart@qq.com>
2018-02-07 16:56:11 +0800

    test

--- src/manage/components/secCandy/dataTable.vue
+++ src/manage/components/secCandy/dataTable.vue
@@ -3,14 +3,14 @@
         <el-table align="center" v-loading="loading" ref="multipleTable" :data="dataList" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
             <el-table-column type="selection" width="55">
             </el-table-column>
-            <el-table-column prop="passiveWallet" label="被分享(钱包地址)" width="380">
+            <el-table-column prop="passiveWallet" label="分享者(钱包地址)" width="380">
                 <template slot-scope="scope">
                     {{scope.row.passiveWallet.walletId}}
                 </template>
             </el-table-column>
             <!-- <el-table-column prop="passiveCode" label="分享码" >
             </el-table-column> -->
-            <el-table-column prop="wallets" label="被分享总数" width="100">
+            <el-table-column prop="wallets" label="分享总数" width="100">
                 <template slot-scope="scope">
                     {{scope.row.wallets.length}}
                 </template>
