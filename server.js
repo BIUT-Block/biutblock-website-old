460aa465854d8c440aa4603698717ca3693398c3
xiaoshen746 <doramart@qq.com>
2018-02-07 13:12:08 +0800

    tesst

--- server.js
+++ server.js
@@ -36,11 +36,33 @@ const system = require('./server/routers/system');
 const message = require('./server/routers/message');
 
 // 机器人配置
-// const botClient = require('./botClient');
-// botClient.setWebhook(settings.BASE_URL + '/' + webhookToken);
-
 const botClient = new Telegraf(settings.TELEGRAM_API_TOKEN)
-botClient.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hey there!</b>'))
+bot.on('text', async ({ message, replyWithHTML }) => {
+    if (message) {
+        let currentCode = (message.text).trim();
+        if (shortid.isValid(currentCode)) {
+            try {
+                let myWallet = await SecCandyLog.checkCurrentCode(currentCode);
+                if (myWallet && myWallet._id) {
+                    if (myWallet.hasSend) {
+                        replyWithHTML('Your code: ' + myWallet.myCode + ', Failed. Each Telegram user can only be verified once. 你的验证码：' + myWallet.myCode + '，校验失败，每个Telegram用户仅可校验一次。')
+                    } else {
+                        let currentLink = "https://www.secblock.io/referral?code=" + currentCode;
+                        let shareWords = "Your verification code: " + myWallet.myCode + ", SUCCESS & DONE!Join the SEC\u2019s official Telegram group to get 20 SEC awards; Each successful invitation to a user can also receive an additional 20 SEC awards, with a maximum of 600SEC per person, waiting to be dropped.Your share link: " + currentLink + ". 你的验证码是: " + myWallet.myCode + "，校验成功！加入SEC官方Telegram群即可获得20个SEC奖励；每成功邀请一个用户，还可额外获得20个SEC奖励，每人最多可得600SEC，坐等空投，送完即止。你的分享链接：" + currentLink + "";
+                        console.log('校验分享码成功', shareWords);
+                        replyWithHTML(shareWords)
+                        // 标记已关注群并发送
+                        await SecCandyLog.activeUserWallet(currentCode);
+                    }
+                }
+            } catch (error) {
+                console.log('校验机器人消息异常', error);
+            }
+        }
+    } else {
+        console.log('can not receive messages');
+    }
+})
 // Set telegram webhook
 let webhookToken = settings.WEBHOOK_TOKEN || randomstring.generate(16);
 botClient.telegram.setWebhook(settings.BASE_URL + '/' + webhookToken)
