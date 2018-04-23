e9409954bf0c6acf20d0ad025e8255f9ff6bc350
肖燊 <dora@xiaoshendeMini.lan>
2018-04-23 23:31:37 +0800

    test

--- src/manage/components/regUser/dataTable.vue
+++ src/manage/components/regUser/dataTable.vue
@@ -7,7 +7,7 @@
             </el-table-column>
             <el-table-column prop="date" label="注册时间" show-overflow-tooltip>
             </el-table-column>
-            <el-table-column prop="email" label="邮箱" show-overflow-tooltip>
+            <el-table-column prop="phoneNum" label="手机号" show-overflow-tooltip>
             </el-table-column>
             <el-table-column label="操作" width="150">
                 <template slot-scope="scope">
